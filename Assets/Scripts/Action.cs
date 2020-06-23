d506d2512a57229cf86e36f670b1bd723087f6a6
Daniel Fróes <danielfroes15@gmail.com>
2020-06-23 09:14:27 -0300

    action menu select fixed and start of the action system

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ActionType {
+    attack,
+    spell,
+    item
+};
+
+public enum EffectType {
+    damage,
+    heal
+}
+
+[CreateAssetMenu(fileName = "new Action", menuName = "Action")]
+public class Action : ScriptableObject
+{
+    public string actionName;
+    public ActionType actionType;
+    public EffectType effectType;
+    public float castVelocity;
+    public int actionBarsNeeded;
+    
+    //jeito de triggar animação
+}
--- Assets/Scripts/ActionMenu.cs
+++ Assets/Scripts/ActionMenu.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ActionSystem : MonoBehaviour
+using UnityEngine.EventSystems;
+public class ActionMenu : MonoBehaviour
 {
     [SerializeField] private GameObject actionMenu;
-
+    private EventSystem es;
+    private void Awake() {
+       es = GameObject.FindObjectOfType<EventSystem>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,12 +23,17 @@ public class ActionSystem : MonoBehaviour
 
     private void ToggleActionMenu()
     {
-
         actionMenu.SetActive(!actionMenu.activeInHierarchy);
         
         if(actionMenu.activeInHierarchy)
+        {
             Time.timeScale = 0f;
+            es.SetSelectedGameObject(null);
+            es.SetSelectedGameObject(es.firstSelectedGameObject);
+        }
         if(!actionMenu.activeInHierarchy)
+        {
             Time.timeScale = 1f;
+        }
     }
 }

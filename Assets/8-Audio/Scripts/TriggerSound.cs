9f3ed6f39a608853274437e6df2ce707913e1c84
murilocarrijosilva <murilocarrijosilva@gmail.com>
2021-02-27 18:27:21 -0300

    sons dos ataques

---
+++ Assets/8-Audio/Scripts/TriggerSound.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour
+{
+    public Sound sound;
+
+    private void OnEnable()
+    {
+        AudioManager.Play(sound);
+    }
+}

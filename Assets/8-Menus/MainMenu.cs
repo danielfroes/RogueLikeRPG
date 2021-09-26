0c5929dbcd4230a0c67eec225bb14bcb814049f2
trashbait <enricommandelli@gmail.com>
2021-09-26 14:15:26 -0300

    menu

---
+++ Assets/8-Menus/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button defaultButton;
+    public Button backButton;
+    
+    void Start(){
+        Debug.Log("start");
+        SelectDefault();
+    }
+
+    public void SelectDefault(){
+        if (defaultButton != null){
+            defaultButton.Select();
+        }
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void SelectBack(){
+        if (backButton != null){
+            backButton.Select();
+        }
+    }
+}
